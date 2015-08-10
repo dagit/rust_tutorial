a062d1f74f132961c2ad4f60ddb29c71784eb283
Jason Dagit <dagit@galois.com>
2015-08-09 23:41:29 -0700

    next step in the tutorial

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,15 @@ use tcod::{Console, RootConsole, BackgroundFlag};
 use tcod::input::Key::Special;
 use tcod::input::KeyCode::{Escape, Up, Down, Left, Right};
 
-fn render(con: &mut RootConsole, x: i32, y: i32, dog_x: i32, dog_y: i32) {
+struct Point {
+  x: i32,
+  y: i32
+}
+
+fn render(con: &mut RootConsole, c_point: &Point, d_point: &Point) {
   con.clear();
-  con.put_char(x, y, '@', BackgroundFlag::Set);
-  con.put_char(dog_x, dog_y, 'd', BackgroundFlag::Set);
+  con.put_char(c_point.x, c_point.y, '@', BackgroundFlag::Set);
+  con.put_char(d_point.x, d_point.y, 'd', BackgroundFlag::Set);
   con.flush();
 }
 
@@ -17,16 +22,14 @@ fn main() {
   let mut rng = rand::thread_rng();
   let con_x = 80i32;
   let con_y = 50i32;
-  let mut char_x = 40i32;
-  let mut char_y = 25i32;
-  let mut dog_x = 10i32;
-  let mut dog_y = 10i32;
+  let mut char_point = Point { x: 40, y: 25 };
+  let mut dog_point  = Point { x: 10, y: 10 };
   let mut con = RootConsole::initializer()
     .size(con_x, con_y)
     .title("libtcod Rust tutorial")
     .init();
   let mut exit = false;
-  render(&mut con, char_x, char_y, dog_x, dog_y);
+  render(&mut con, &char_point, &dog_point);
   while !(con.window_closed() || exit) {
     // wait for user input
     let keypress = con.wait_for_keypress(true);
@@ -35,39 +38,39 @@ fn main() {
     match keypress.key {
       Special(Escape) => exit = true,
       Special(Up)     => {
-        if char_y >= 1 {
-          char_y -= 1;
+        if char_point.y >= 1 {
+          char_point.y -= 1;
         }
       },
       Special(Down)   => {
-        if char_y < (con_y -1) {
-          char_y += 1;
+        if char_point.y < (con_y -1) {
+          char_point.y += 1;
         }
       },
       Special(Left) => {
-        if char_x >= 1 {
-          char_x -= 1;
+        if char_point.x >= 1 {
+          char_point.x -= 1;
         }
       },
       Special(Right) => {
-        if char_x < (con_x - 1) {
-          char_x += 1;
+        if char_point.x < (con_x - 1) {
+          char_point.x += 1;
         }
       },
       _               => {}
     }
 
     let offset_x = between.sample(&mut rng) - 1;
-    if(dog_x + offset_x) > 0 && (dog_x + offset_x) < (con_x - 1){
-      dog_x += offset_x;
+    if(dog_point.x + offset_x) > 0 && (dog_point.x + offset_x) < (con_x - 1){
+      dog_point.x += offset_x;
     }
 
     let offset_y = between.sample(&mut rng) - 1;
-    if(dog_y + offset_y) > 0 && (dog_y + offset_y) < (con_y -1){
-      dog_y += offset_y;
+    if(dog_point.y + offset_y) > 0 && (dog_point.y + offset_y) < (con_y -1){
+      dog_point.y += offset_y;
     }
 
     // render
-    render(&mut con, char_x, char_y, dog_x, dog_y);
+    render(&mut con, &char_point, &dog_point);
   }
 }
