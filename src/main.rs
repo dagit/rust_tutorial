c1abfe8960aab15fdb115444b75b2d61b66b1bd3
Jason Dagit <dagit@galois.com>
2015-08-09 22:53:14 -0700

    next step in the tutorial

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,30 @@ use tcod::{Console, RootConsole, BackgroundFlag};
 use tcod::input::Key::Special;
 use tcod::input::KeyCode::{Escape};
 
+fn render(con: &mut RootConsole) {
+  con.clear();
+  con.put_char(40, 25, '@', BackgroundFlag::Set);
+  con.flush();
+}
+
 fn main() {
   let mut con = RootConsole::initializer()
     .size(80, 50)
     .title("libtcod Rust tutorial")
     .init();
   let mut exit = false;
+  render(&mut con);
   while !(con.window_closed() || exit) {
-    con.clear();
-    con.put_char(40, 25, '@', BackgroundFlag::Set);
-    con.flush();
+    // wait for user input
     let keypress = con.wait_for_keypress(true);
+
+    // update game state
     match keypress.key {
       Special(Escape) => exit = true,
-      _ => {}
+      _               => {}
     }
+
+    // render
+    render(&mut con);
   }
 }
