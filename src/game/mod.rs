9ebcc8320b323aa30636b5884a238292ed5d84c3
Jason Dagit <dagit@galois.com>
2015-08-10 23:09:39 -0700

    Update to next tutorial step

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,13 +1,63 @@
 extern crate rand;
+extern crate tcod;
 
 use std::cell::RefCell;
 use self::rand::ThreadRng;
 
-use util::Bound;
+use self::tcod::RootConsole;
+use self::tcod::input::KeyState;
 
-pub struct Game {
+use util::{Bound, Point};
+use rendering::{RenderingComponent, TcodRenderingComponent};
+use traits::{Updates, Updatable};
+use character::Character;
+
+pub struct Game<'a> {
   pub exit: bool,
   pub window_bounds: Bound,
-  pub rng: RefCell<ThreadRng>
+  pub rng: RefCell<ThreadRng>,
+  pub rendering_component: Box<RenderingComponent + 'a>
+}
+
+impl<'a> Game<'a> {
+  pub fn new() -> Game<'a> {
+    let bound = Bound {
+        min: Point { x: 0, y: 0 },
+        max: Point { x: 79, y: 49 }
+    };
+    let con = RootConsole::initializer()
+      .size(bound.max.x+1, bound.max.y+1)
+      .title("libtcod Rust tutorial")
+      .init();
+    let rc = TcodRenderingComponent { console: RefCell::new(con) };
+    Game {
+      exit: false,
+      window_bounds: bound,
+      rng: RefCell::new(rand::thread_rng()),
+      rendering_component: Box::new(rc)
+    }
+  }
+
+  pub fn render(&self,
+                npcs: &Vec<Updatable<'a>>,
+                c: &Character) {
+    self.rendering_component.before_render_new_frame();
+    for i in npcs.iter() {
+      i.borrow().render(&*self.rendering_component);
+    }
+    c.render(&*self.rendering_component);
+    self.rendering_component.after_render_new_frame();
+  }
+
+  pub fn update(&self, npcs: &Vec<Updatable<'a>>, c: &mut Character, keypress: KeyState) {
+    c.update(keypress, self);
+    for i in npcs.iter() {
+      i.borrow_mut().update(self);
+    }
+  }
+
+  pub fn wait_for_keypress(&self) -> KeyState {
+    self.rendering_component.wait_for_keypress()
+  }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,68 +2,35 @@ extern crate tcod;
 extern crate rand;
 extern crate rl;
 
-use tcod::RootConsole;
 use tcod::input::Key::Special;
 use tcod::input::KeyCode::Escape;
-use tcod::input::KeyState;
-use std::cell::RefCell;
 
-use rl::util::{Point, Bound};
 use rl::game::Game;
 use rl::character::Character;
 use rl::npc::NPC;
 use rl::traits::{Updates, Updatable, mk_updatable};
-use rl::rendering::{RenderingComponent, TcodRenderingComponent};
-
-fn render<'a>(rendering_component: &RenderingComponent,
-              npcs: &Vec<Updatable<'a>>,
-              c: &Character) {
-  rendering_component.before_render_new_frame();
-  for i in npcs.iter() {
-    i.borrow().render(rendering_component);
-  }
-  c.render(rendering_component);
-  rendering_component.after_render_new_frame();
-}
-
-fn update<'a>(npcs: &Vec<Updatable<'a>>, c: &mut Character, keypress: KeyState, game: &Game) {
-  c.update(keypress, &game);
-  for i in npcs.iter() {
-    i.borrow_mut().update(&game);
-  }
-}
+use rl::rendering::{RenderingComponent};
 
 fn main() {
-  let mut game = Game {
-    exit: false,
-    window_bounds: Bound {
-      min: Point { x: 0, y: 0 },
-      max: Point { x: 79, y: 49 } },
-    rng: RefCell::new(rand::thread_rng())
-    };
   let mut c = Character::new(40, 25, '@');
   let npcs: Vec<Updatable> = vec![
     mk_updatable(NPC::new(10, 10, 'd')),
     mk_updatable(NPC::new(40, 25, 'c')),
     ];
-  let con = RootConsole::initializer()
-    .size(game.window_bounds.max.x+1, game.window_bounds.max.y+1)
-    .title("libtcod Rust tutorial")
-    .init();
-  let rendering_component = TcodRenderingComponent { console: RefCell::new(con) };
-  render(&rendering_component, &npcs, &c);
-  while !(rendering_component.window_closed() || game.exit) {
+  let mut game = Game::new();
+  game.render(&npcs, &c);
+  while !(game.rendering_component.window_closed() || game.exit) {
     // wait for user input
-    let keypress = rendering_component.wait_for_keypress();
+    let keypress = game.rendering_component.wait_for_keypress();
 
     // update game state
     match keypress.key {
       Special(Escape) => game.exit = true,
       _               => {}
     }
-    update(&npcs, &mut c, keypress, &game);
+    game.update(&npcs, &mut c, keypress);
 
     // render
-    render(&rendering_component, &npcs, &c);
+    game.render(&npcs, &c);
   }
 }
